6501940f337da08e57dda1bac26526f352662626
lenicolegabriel <lykimhout@gmail.com>
2025-01-28 12:19:03 +0700

    Update script.js

--- trade/js/script.js
+++ trade/js/script.js
@@ -1,6 +1,7 @@
 /* Place your JavaScript in this file */
 
 $(document).ready(function(){
+	console.log("test");
 	$.getJSON("https://lykimhout.github.io/konkhmer/trade/database/data.json", function(json) {
 		console.log(json); 
 	});
