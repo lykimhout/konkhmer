ffd21e1412a84c0e26185952551b5565ee7208da
lenicolegabriel <lykimhout@gmail.com>
2025-01-28 12:42:32 +0700

    ds

--- trade/js/script.js
+++ trade/js/script.js
@@ -28,6 +28,6 @@ $(document).ready(function(){
 	console.log("test");
 	
 	$.getJSON("https://lykimhout.github.io/konkhmer/trade/database/data.json", function(json) {
-		console.log(json); 
+		
 	});
 });
